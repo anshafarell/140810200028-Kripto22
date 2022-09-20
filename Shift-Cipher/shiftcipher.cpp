199ba6f0068649d40955bce986ec476f17783ffb
anshafarell <anshafarell@gmail.com>
2022-09-20 18:01:52 +0700

    Submit

---
+++ Shift-Cipher/shiftcipher.cpp
@@ -0,0 +1,74 @@
+/*
+    Nama    : Ananda Shaquille Farell
+    NPM     : 140810200028
+    MK      : Kriptografi (Praktikum)
+    Hal     : Program Shift Cipher
+*/
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+using namespace std;
+
+int main(){
+    system("CLS");
+    cout << "+----------------------+\n";
+    cout << "| PROGRAM SHIFT CIPHER |\n";
+    cout << "+----------------------+\n\n";
+    cout << "Teks  : ";
+    char teks[100];
+    cin.getline(teks,100);
+    int i, j, length, pilihan, kunci;
+    cout << "Kunci : ";
+    cin >> kunci;
+    cout << "\n";
+    length = strlen(teks);
+    cout << "Pilihan: \n1. Enkripsi \n2. Dekripsi \n";
+    cout << "Pilihan Anda: ";
+    cin >> pilihan;
+    cout << "\n";
+    if(pilihan == 1){
+        char ch;
+        for(int i = 0; teks[i] != '\0'; ++i){
+            ch = teks[i];
+            if(ch >= 'a' && ch <= 'z'){
+                ch = ch + kunci;
+                if (ch > 'z') {
+                ch = ch - 'z' + 'a' - 1;
+                }  
+                teks[i] = ch;
+            }
+            else if(ch >= 'A' && ch <= 'Z'){
+                ch = ch + kunci;
+                if (ch > 'Z'){
+                ch = ch - 'Z' + 'A' - 1;
+                }
+                teks[i] = ch;
+            }
+        }
+        printf("Hasil Enkripsi: %s", teks);
+        cout << "\n";
+    }
+    else if(pilihan == 2){
+        char ch;
+        for(int i = 0; teks[i] != '\0'; ++i){
+            ch = teks[i];
+            if(ch >= 'a' && ch <= 'z') {
+                ch = ch - kunci;
+                if(ch < 'a'){
+                ch = ch + 'z' - 'a' + 1;
+                }
+                teks[i] = ch;
+            }
+            else if(ch >= 'A' && ch <= 'Z') {
+                ch = ch - kunci;
+                if(ch < 'A') {
+                ch = ch + 'Z' - 'A' + 1;
+                }
+                teks[i] = ch;
+            }
+        }
+        cout << "Hasil Dekripsi: " << teks << "\n";
+    }
+    cout << "\n";
+    return 0;
+}
